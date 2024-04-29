b7766b761dd4b292fd53627c0d8f1477a13315a5
dellaquila05 <129840421+dellaquila05@users.noreply.github.com>
2024-04-29 11:34:18 +0000

    implementazione html base, vanno aggiunte le immagini e i caroselli vari

---
+++ index.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const express = require("express");
+const http = require("http");
+const app = express();
+const path = require("path");
+const bodyParser = require("body-parser");
+const { Server } = require("socket.io");
+const conf = JSON.parse(fs.readFileSync("./conf.json"));
+const server = http.createServer(app);
+const io = new Server(server);
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  }),
+);
+app.use("/", express.static(path.join(__dirname, "public")));
+server.listen(conf.port, () => {
+  console.log("server running on port: " + conf.port);
+});
